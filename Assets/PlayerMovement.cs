d3523d980c5d13e87a1f5ec2d034be517a717398
sgaleraalq <sgaleraalq@gmail.com>
2024-10-25 19:55:35 +0200

    First functions created

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -3,14 +3,12 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+
+    public Rigidbody rb;
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        rb.AddForce(0, 0, 2000 * Time.deltaTime);
     }
 }
