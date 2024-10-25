c32f24fe3ea09a0bde5dcd7e2bc313c7d83566d2
sgaleraalq <sgaleraalq@gmail.com>
2024-10-25 23:40:47 +0200

    Movement class

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -6,9 +6,21 @@ public class PlayerMovement : MonoBehaviour
 
     public Rigidbody rb;
 
+    public float forwardForce = 2000f;
+    public float sidewaysForce = 500f;
+
     // Update is called once per frame
     void FixedUpdate()
     {
-        rb.AddForce(0, 0, 2000 * Time.deltaTime);
+        rb.AddForce(0, 0, forwardForce * Time.deltaTime);
+
+        if ( Input.GetKey(KeyCode.D) )
+        {
+            rb.AddForce(sidewaysForce * Time.deltaTime, 0, 0);
+        }
+        if ( Input.GetKey(KeyCode.A) )
+        {
+            rb.AddForce(-sidewaysForce * Time.deltaTime, 0, 0);
+        }
     }
 }
